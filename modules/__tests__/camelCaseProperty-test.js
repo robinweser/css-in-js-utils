a2e79b4f43129089c00d1c73dcfa65a2fd5a386a
Oleksandr Fediashov <olfedias@microsoft.com>
2020-05-26 10:03:28 +0200

    perf: improve camelCaseProperty

--- modules/__tests__/camelCaseProperty-test.js
+++ modules/__tests__/camelCaseProperty-test.js
@@ -3,9 +3,17 @@ import camelCaseProperty from '../camelCaseProperty'
 describe('Camel casing properties', () => {
   it('should camel case properties', () => {
     expect(camelCaseProperty('transition-delay')).toEqual('transitionDelay')
+
     expect(camelCaseProperty('-webkit-transition-delay')).toEqual(
       'WebkitTransitionDelay'
     )
     expect(camelCaseProperty('-ms-transition')).toEqual('msTransition')
+
+    expect(camelCaseProperty('WebkitTransitionDelay')).toEqual(
+      'WebkitTransitionDelay'
+    )
+    expect(camelCaseProperty('WebkitFlex')).toEqual(
+      'WebkitFlex'
+    )
   })
 })
--- modules/camelCaseProperty.js
+++ modules/camelCaseProperty.js
@@ -1,10 +1,23 @@
-const DASH = /-([a-z])/g
-const MS = /^Ms/g
+function toLowerCaseFirstCharacter(value) {
+  return value.substr(0, 1).toLowerCase()  + value.substr(1);
+}
 
-function toUpper(match) {
-  return match[1].toUpperCase()
+function toUpperCaseFirstCharacter(value) {
+  return value.substr(0, 1).toUpperCase() + value.substr(1);
 }
 
 export default function camelCaseProperty(property) {
-  return property.replace(DASH, toUpper).replace(MS, 'ms')
+  // -ms-transition => ['', 'ms', 'transition']
+  // transition-delay => ['transition', 'delay']
+  // WebkitTransitionDelay => ['WebkitTransitionDelay']
+  const separatedByDash = property.split('-')
+
+  if (separatedByDash.length === 1) {
+    return property;
+  }
+
+  const inPascalCase = separatedByDash.map(toUpperCaseFirstCharacter).join('');
+  const shouldLowerCaseFirstChar = separatedByDash[0] !== '' || separatedByDash[1] === 'ms'
+
+  return shouldLowerCaseFirstChar ? toLowerCaseFirstCharacter(inPascalCase) : inPascalCase;
 }
